7f25a11a657226547274137b5fb4e8593c001b69
Sergey Protko <sergey.protko@yandex.ru>
2025-01-17 19:55:27 +0300

    update parse desktop file

--- pkg/parsedesktopfile/main.go
+++ pkg/parsedesktopfile/main.go
@@ -1,9 +1,72 @@
 package parsedesktopfile
 
-func GetFromString(desktop string) {
-	// TODO
+import (
+	"os"
+	"strings"
+
+	"github.com/probeldev/fastlauncher/pkg/parsedesktopfile/model"
+)
+
+type parseDesktopFile struct{}
+
+func GetParseDesktopFile() parseDesktopFile {
+	p := parseDesktopFile{}
+
+	return p
 }
 
-func GetFromFile(desktop string) {
-	// TODO
+func (p *parseDesktopFile) GetFromString(desktop string) model.Desktop {
+	return p.parse(desktop)
+}
+
+func (p *parseDesktopFile) GetFromFile(desktopFile string) (model.Desktop, error) {
+	response := model.Desktop{}
+	body, err := os.ReadFile(desktopFile)
+
+	if err != nil {
+		return response, err
+	}
+
+	return p.parse(string(body)), nil
+}
+
+func (p *parseDesktopFile) parse(body string) model.Desktop {
+	response := model.Desktop{}
+
+	bodyLines := strings.Split(body, "\n")
+
+	mapLines := map[string]string{}
+	for _, line := range bodyLines {
+		if line == "" {
+			continue
+		}
+		if strings.HasPrefix(line, "[") {
+			continue
+		}
+
+		lineArr := strings.Split(line, "=")
+
+		mapLines[lineArr[0]] = lineArr[1]
+	}
+
+	if exec, ok := mapLines["exec"]; ok {
+		response.Exec = exec
+	}
+	if name, ok := mapLines["name"]; ok {
+		response.Name = name
+	}
+	if typeDesk, ok := mapLines["type"]; ok {
+		response.Type = typeDesk
+	}
+	if comment, ok := mapLines["comment"]; ok {
+		response.Comment = comment
+	}
+	if keywords, ok := mapLines["keywords"]; ok {
+		response.Keywords = keywords
+	}
+	if terminal, ok := mapLines["terminal"]; ok {
+		response.Terminal = terminal == "true"
+	}
+
+	return response
 }
---
+++ pkg/parsedesktopfile/model/desktopfile.go
@@ -0,0 +1,12 @@
+package model
+
+type Desktop struct {
+	Type string
+	Exec string
+
+	Terminal bool
+	Keywords string
+
+	Name    string
+	Comment string
+}

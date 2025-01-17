acfa26ea080d41df77d11cc4cf3ca89bb8c70143
Sergey Protko <sergey.protko@yandex.ru>
2025-01-17 20:10:19 +0300

    add test for parse desktop file

--- pkg/parsedesktopfile/main.go
+++ pkg/parsedesktopfile/main.go
@@ -15,11 +15,11 @@ func GetParseDesktopFile() parseDesktopFile {
 	return p
 }
 
-func (p *parseDesktopFile) GetFromString(desktop string) model.Desktop {
+func (p *parseDesktopFile) ParseFromString(desktop string) model.Desktop {
 	return p.parse(desktop)
 }
 
-func (p *parseDesktopFile) GetFromFile(desktopFile string) (model.Desktop, error) {
+func (p *parseDesktopFile) ParseFromFile(desktopFile string) (model.Desktop, error) {
 	response := model.Desktop{}
 	body, err := os.ReadFile(desktopFile)
 
@@ -37,6 +37,8 @@ func (p *parseDesktopFile) parse(body string) model.Desktop {
 
 	mapLines := map[string]string{}
 	for _, line := range bodyLines {
+		line = strings.Trim(line, " ")
+		line = strings.Trim(line, "\t")
 		if line == "" {
 			continue
 		}
@@ -49,22 +51,22 @@ func (p *parseDesktopFile) parse(body string) model.Desktop {
 		mapLines[lineArr[0]] = lineArr[1]
 	}
 
-	if exec, ok := mapLines["exec"]; ok {
+	if exec, ok := mapLines["Exec"]; ok {
 		response.Exec = exec
 	}
-	if name, ok := mapLines["name"]; ok {
+	if name, ok := mapLines["Name"]; ok {
 		response.Name = name
 	}
-	if typeDesk, ok := mapLines["type"]; ok {
+	if typeDesk, ok := mapLines["Type"]; ok {
 		response.Type = typeDesk
 	}
-	if comment, ok := mapLines["comment"]; ok {
+	if comment, ok := mapLines["Comment"]; ok {
 		response.Comment = comment
 	}
-	if keywords, ok := mapLines["keywords"]; ok {
+	if keywords, ok := mapLines["Keywords"]; ok {
 		response.Keywords = keywords
 	}
-	if terminal, ok := mapLines["terminal"]; ok {
+	if terminal, ok := mapLines["Terminal"]; ok {
 		response.Terminal = terminal == "true"
 	}
 
---
+++ pkg/parsedesktopfile/test/main_test.go
@@ -0,0 +1,44 @@
+package parsedesktopfile_test
+
+import (
+	"testing"
+
+	"github.com/probeldev/fastlauncher/pkg/parsedesktopfile"
+	"github.com/probeldev/fastlauncher/pkg/parsedesktopfile/model"
+)
+
+func TestParseFromString(t *testing.T) {
+	fn := "TestParseFromString"
+	body := `
+[Desktop Entry]
+Type=Application
+Exec=foot
+Icon=foot
+Terminal=false
+Categories=System;TerminalEmulator;
+Keywords=shell;prompt;command;commandline;
+
+Name=Foot
+GenericName=Terminal
+Comment=A wayland native terminal emulator
+	`
+	expected := model.Desktop{
+		Type: "Application",
+		Exec: "foot",
+
+		Terminal: false,
+		Keywords: "shell;prompt;command;commandline;",
+
+		Name:    "Foot",
+		Comment: "A wayland native terminal emulator",
+	}
+
+	parse := parsedesktopfile.GetParseDesktopFile()
+
+	actual := parse.ParseFromString(body)
+
+	if actual != expected {
+		t.Errorf(fn, "not equal", actual, parse)
+	}
+
+}

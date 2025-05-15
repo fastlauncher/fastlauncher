338b0696f7255c506118db502b78e4ee9a5e248f
Sergey Protko <sergey.protko@yandex.ru>
2025-05-15 15:28:26 +0300

    update package.nix

--- package.nix
+++ package.nix
@@ -4,5 +4,5 @@
 buildGoModule {
   name = "fastlauncher";
   src = ./.;
-  vendorHash = "sha256-eJ3yXIAATKfJCXBkE7IGhicWYPxfTliZJRZ8Q1BNX/I=";
+  vendorHash = "sha256-b0bu8PuXsf2JHIvfdWTdfx9MVYZsyhsmAfYkgZ3/mLA=";
 }
